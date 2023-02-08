74e58078a7e11bbbe385ae93153745033e98cb53
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-08 10:42:39 -0500

    Ajout de la classe Attaquant

---
+++ MarioNRabbit/MarioNRabbit/Models/Attaquant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioNRabbit.Models
+{
+    internal class Attaquant : Personnage
+    {
+        public Attaquant(string pNom, int pPositionX, int pPositionY, int pNbCasesDeplacementMax, int pNbPointsVie) : base(pNom, pPositionX, pPositionY, pNbCasesDeplacementMax, pNbPointsVie)
+        {
+        }
+
+        public bool Attaquer(Personnage pPersoAttaquant, Personnage pPersoCible)
+        {
+            return true;
+        }
+            
+    }
+}
