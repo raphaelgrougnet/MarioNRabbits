27d0e55312054726fbbeac464d4a7d17fe12a44b
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-08 09:09:40 -0500

    Ajout des classes de personnages

---
+++ MarioNRabbit/MarioNRabbit/Models/Hero.cs
@@ -0,0 +1,34 @@
+﻿using BattleKingdom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioNRabbit.Models
+{
+    class Hero : Personnage, IAttaquer
+    {
+
+        #region CONSTANTES
+
+        #endregion
+
+        #region ATTRIBUTS
+
+        #endregion
+
+        #region PROPRIÉTÉS
+
+        #endregion
+
+        #region CONSTRUCTEURS
+
+        #endregion
+
+        #region MÉTHODES
+
+        #endregion
+
+    }
+}
