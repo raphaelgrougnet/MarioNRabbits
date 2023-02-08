7068af23260812273c96fa96f928932f87d1783e
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-08 09:58:30 -0500

    Ajout de l'interface FamilleMario

---
+++ MarioNRabbit/MarioNRabbit/Models/IFamilleMario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioNRabbit.Models
+{
+    public interface IFamilleMario
+    {
+        public CompetenceSpeciale Competence { get; set; }
+    }
+}
