b7fcb556cf6c3d6112c19ed01d873c28eae1d761
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-08 09:57:23 -0500

    Ajout de la classe CompetenceSpeciale

---
+++ MarioNRabbit/MarioNRabbit/Models/CompetenceSpeciale.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioNRabbit.Models
+{
+    public abstract class CompetenceSpeciale
+    {
+        
+        #region CONSTANTES
+
+        #endregion
+
+        #region ATTRIBUTS
+
+        #endregion
+
+        #region PROPRIÉTÉS
+
+        #endregion
+
+        #region CONSTRUCTEURS
+
+        #endregion
+
+        #region MÉTHODES
+
+        #endregion
+            
+    }
+}
