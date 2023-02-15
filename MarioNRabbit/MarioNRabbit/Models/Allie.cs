7c086d4f9911cbd0004ed27a711e948de04678d9
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-02-15 11:39:12 -0500

    Rajout de la classe

---
+++ MarioNRabbit/MarioNRabbit/Models/Allie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioNRabbit.Models
+{
+    public class Allie : Personnage
+    {
+
+        #region CONSTANTES
+
+        #endregion
+
+        #region ATTRIBUTS
+
+        #endregion
+
+        #region PROPRIÉTÉS
+
+        #endregion
+
+        #region CONSTRUCTEURS
+        /// <summary>
+        /// Constructeur de Allie
+        /// </summary>
+        /// <param name="pNom"></param>
+        /// <param name="pPositionX"></param>
+        /// <param name="pPositionY"></param>
+        /// <param name="pNbCasesDeplacementMax"></param>
+        /// <param name="pNbPointsVie"></param>
+        public Allie(string pNom, int pPositionX, int pPositionY, int pNbCasesDeplacementMax, int pNbPointsVie) : base(pNom, pPositionX, pPositionY, pNbCasesDeplacementMax, pNbPointsVie)
+        {
+        }
+        #endregion
+
+        #region MÉTHODES
+
+        #endregion
+
+
+    }
+}
